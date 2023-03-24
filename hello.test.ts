957f6ad2a79254479b666b62a6a8d9dbcb2a97e6
Simon Green Kristensen <git@simon-green.dev>
2023-03-24 14:26:17 +0100

    Got fuzzy matching on provided completions

--- hello.test.ts
+++ hello.test.ts
@@ -2,54 +2,47 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+/* eslint-disable local/code-layering, local/code-import-patterns */
 import * as assert from "assert";
 import { spawn } from "child_process";
 import { readFileSync } from "fs";
 import type { Readable, Writable } from "stream";
-import { EditorOptions } from "vs/editor/common/config/editorOptions";
-import { IPosition } from "vs/editor/common/core/position";
+import { Event } from "vs/base/common/event";
+import { DisposableStore } from "vs/base/common/lifecycle";
 import * as languages from "vs/editor/common/languages";
-import { CompletionModel } from "vs/editor/contrib/suggest/browser/completionModel";
+import { LanguageService } from "vs/editor/common/services/languageService";
 import {
-	CompletionItem,
-	provideSuggestionItems,
-} from "vs/editor/contrib/suggest/browser/suggest";
-import { WordDistance } from "vs/editor/contrib/suggest/browser/wordDistance";
+	ILanguageConfigurationService,
+	ResolvedLanguageConfiguration,
+} from "vs/editor/common/languages/languageConfigurationRegistry";
+import { TextModel } from "vs/editor/common/model/textModel";
+import type { IUndoRedoService } from "vs/platform/undoRedo/common/undoRedo";
+import {
+	createTestCodeEditor,
+	ITestCodeEditor,
+} from "vs/editor/test/browser/testCodeEditor";
+import { ILanguageFeaturesService } from "vs/editor/common/services/languageFeatures";
+import { LanguageFeaturesService } from "vs/editor/common/services/languageFeaturesService";
+import { ServiceCollection } from "vs/platform/instantiation/common/serviceCollection";
+import { ITelemetryService } from "vs/platform/telemetry/common/telemetry";
+import { NullTelemetryService } from "vs/platform/telemetry/common/telemetryUtils";
+import { ILogService, NullLogService } from "vs/platform/log/common/log";
+import {
+	InMemoryStorageService,
+	IStorageService,
+} from "vs/platform/storage/common/storage";
+import { MockKeybindingService } from "vs/platform/keybinding/test/common/mockKeybindingService";
+import { IKeybindingService } from "vs/platform/keybinding/common/keybinding";
+import { IEditorWorkerService } from "vs/editor/common/services/editorWorker";
+import { mock } from "vs/base/test/common/mock";
+import { ISuggestMemoryService } from "../../browser/suggestMemory";
+import { IMenu, IMenuService } from "vs/platform/actions/common/actions";
+import { ILabelService } from "vs/platform/label/common/label";
+import { IWorkspaceContextService } from "vs/platform/workspace/common/workspace";
+import { SuggestController } from "../../browser/suggestController";
+import { Position } from "vs/editor/common/core/position";
+import { ILanguageService } from "vs/editor/common/languages/language";
 
-export function createSuggestItem(
-	label: string | languages.CompletionItemLabel,
-	overwriteBefore: number,
-	kind = languages.CompletionItemKind.Property,
-	incomplete: boolean = false,
-	position: IPosition = { lineNumber: 1, column: 1 },
-	sortText?: string,
-	filterText?: string
-): CompletionItem {
-	const suggestion: languages.CompletionItem = {
-		label,
-		sortText,
-		filterText,
-		range: {
-			startLineNumber: position.lineNumber,
-			startColumn: position.column - overwriteBefore,
-			endLineNumber: position.lineNumber,
-			endColumn: position.column,
-		},
-		insertText: typeof label === "string" ? label : label.label,
-		kind,
-	};
-	const container: languages.CompletionList = {
-		incomplete,
-		suggestions: [suggestion],
-	};
-	const provider: languages.CompletionItemProvider = {
-		provideCompletionItems(): any {
-			return;
-		},
-	};
-
-	return new CompletionItem(position, suggestion, container, provider);
-}
 let ID = 1;
 type Vsc = { stdin: Writable; stdout: Readable };
 function send(vsc: Vsc, method: unknown, params: unknown) {
@@ -88,17 +81,12 @@ class Queue {
 			return;
 		}
 		if (this.partial) {
-			// console.log("partial", this.partial.slice(this.partial.length - 200), msg.slice(0, 200), "\n...\n", msg.slice(msg.length - 200));
 			msg = this.partial + msg;
 		}
 		try {
 			this.enqueueObject(JSON.parse(msg));
 			this.partial = "";
 		} catch (err) {
-			// const position = Number(
-			// 	(err as SyntaxError).message.match(/position (\d+)/)?.[1]
-			// );
-			// console.error(err, msg.slice(position - 100, position + 200));
 			this.partial = msg;
 		}
 	};
@@ -125,67 +113,117 @@ class Queue {
 }
 
 suite("Test algorithms", function () {
-	// const defaultOptions = <InternalSuggestOptions>{
-	// 	insertMode: 'insert',
-	// 	snippetsPreventQuickSuggestions: true,
-	// 	filterGraceful: true,
-	// 	localityBonus: false,
-	// 	shareSuggestSelections: false,
-	// 	showIcons: true,
-	// 	showMethods: true,
-	// 	showFunctions: true,
-	// 	showConstructors: true,
-	// 	showDeprecated: true,
-	// 	showFields: true,
-	// 	showVariables: true,
-	// 	showClasses: true,
-	// 	showStructs: true,
-	// 	showInterfaces: true,
-	// 	showModules: true,
-	// 	showProperties: true,
-	// 	showEvents: true,
-	// 	showOperators: true,
-	// 	showUnits: true,
-	// 	showValues: true,
-	// 	showConstants: true,
-	// 	showEnums: true,
-	// 	showEnumMembers: true,
-	// 	showKeywords: true,
-	// 	showWords: true,
-	// 	showColors: true,
-	// 	showFiles: true,
-	// 	showReferences: true,
-	// 	showFolders: true,
-	// 	showTypeParameters: true,
-	// 	showSnippets: true,
-	// };
-
-	let model: CompletionModel;
+	let model: TextModel;
+	const languageFeaturesService = new LanguageFeaturesService();
+	let languageService: ILanguageService;
+	const disposables = new DisposableStore();
+	const completionItems = {
+		items: [] as languages.CompletionList["suggestions"],
+	};
+	let editor: ITestCodeEditor;
+	let controller: SuggestController;
 
 	setup(function () {
-		model = new CompletionModel(
+		languageService = disposables.add(new LanguageService());
+		const serviceCollection = new ServiceCollection(
+			[ILanguageFeaturesService, languageFeaturesService],
+			[ITelemetryService, NullTelemetryService],
+			[ILogService, new NullLogService()],
+			[IStorageService, new InMemoryStorageService()],
+			[IKeybindingService, new MockKeybindingService()],
 			[
-				createSuggestItem("foo", 3),
-				createSuggestItem("Foo", 3),
-				createSuggestItem("foo", 2),
+				IEditorWorkerService,
+				new (class extends mock<IEditorWorkerService>() {
+					override computeWordRanges() {
+						return Promise.resolve({});
+					}
+				})(),
 			],
-			1,
-			{
-				leadingLineContent: "foo",
-				characterCountDelta: 0,
-			},
-			WordDistance.None,
-			EditorOptions.suggest.defaultValue,
-			EditorOptions.snippetSuggestions.defaultValue,
-			undefined
+			[
+				ISuggestMemoryService,
+				new (class extends mock<ISuggestMemoryService>() {
+					override memorize(): void {}
+					override select(): number {
+						return 0;
+					}
+				})(),
+			],
+			[
+				IMenuService,
+				new (class extends mock<IMenuService>() {
+					override createMenu() {
+						return new (class extends mock<IMenu>() {
+							override onDidChange = Event.None;
+							override dispose() {}
+						})();
+					}
+				})(),
+			],
+			[ILabelService, new (class extends mock<ILabelService>() {})()],
+			[
+				IWorkspaceContextService,
+				new (class extends mock<IWorkspaceContextService>() {})(),
+			]
 		);
+		const onDidChange: Event<any> = Event.None;
+		const langConfig: ILanguageConfigurationService = {
+			onDidChange,
+			register: (x, y) => ({ dispose() {} }),
+			_serviceBrand: undefined,
+			getLanguageConfiguration(languageId) {
+				return new ResolvedLanguageConfiguration("coq", {});
+			},
+		};
+		const no = () => false;
+		model = disposables.add(
+			new TextModel(
+				"",
+				"coq",
+				TextModel.DEFAULT_CREATION_OPTIONS,
+				undefined,
+				{
+					onDidChange,
+					canUndo: no,
+					canRedo: no,
+					removeElements: () => {},
+				} as unknown as IUndoRedoService,
+				languageService,
+				langConfig
+			)
+		);
+		const provider: languages.CompletionItemProvider = {
+			provideCompletionItems() {
+				return { suggestions: completionItems.items };
+			},
+		};
+		languageFeaturesService.completionProvider.register(
+			{ language: "coq" },
+			provider
+		);
+		editor = disposables.add(
+			createTestCodeEditor(model, { serviceCollection })
+		);
+		controller = editor.registerAndInstantiateContribution(
+			SuggestController.ID,
+			SuggestController
+		);
+	});
+	teardown(function () {
+		disposables.clear();
 	});
 
 	test("Test algorithms", async function () {
-		const algo = "algo";
+		const ranking = 0;
 		const file = "MoreBasic.v";
 
-		const vsc = spawn("vscoqtop");
+		const vsc = spawn(
+			"/home/monner/Projects/vscoq/language-server/_build/install/default/bin/vscoqtop",
+			[
+				"-bt",
+				"-coqlib",
+				"/home/monner/Projects/vscoq/language-server/_build/default/coq",
+			]
+		);
 
 		const queue = await new Promise<Queue>((res) => new Queue(res));
 
@@ -194,6 +232,9 @@ suite("Test algorithms", function () {
 		vsc.stdout.on("data", (d) => {
 			const decoded = decoder.decode(d).split(/Content-Length: \d+\r?\n\r?\n/);
 			decoded.forEach(queue.enqueue);
+		});
+		vsc.stderr.on("data", (d) => {
+			console.error(decoder.decode(d));
 		});
 
 		send(vsc, "initialize", {
@@ -207,7 +248,7 @@ suite("Test algorithms", function () {
 					workers: 1,
 					mode: 1,
 				},
-				completionAlgorithm: algo,
+				ranking,
 			},
 		});
 
@@ -216,6 +257,8 @@ suite("Test algorithms", function () {
 
 		const contents = readFileSync(`../${file}`, "utf-8");
 		const lines = contents.split("\n");
+
+		model.setValue(contents);
 
 		send(vsc, "textDocument/didOpen", {
 			textDocument: {
@@ -227,40 +270,62 @@ suite("Test algorithms", function () {
 		const regex = /(apply|rewrite|rewrite <-) ([a-zA-Z_][a-zA-Z_0-9]*)/;
 		const ranks = [];
 
+		const suggestResolver = { res: (d: unknown) => {} };
+
+		controller.model.onDidSuggest(() => suggestResolver.res(undefined));
+
 		for (let i = 0; i < lines.length; i++) {
 			const line = lines[i];
 			let search = line.search(regex);
 			let tacticStart = 0;
 			while (search >= 0) {
 				tacticStart += search;
-				const [sentence, tactic, lemma] =
+				const [_sentence, tactic, lemma] =
 					regex.exec(line.slice(tacticStart)) ?? [];
 				const tacticEnd = tacticStart + tactic.length;
 
+				// TODO: Determine whether we use word under cursor on backend
+				// ...as completion provider is invoked on every suggest trigger
 				send(vsc, "textDocument/completion", {
 					textDocument: {
 						uri: file,
 					},
 					position: { line: i, character: tacticEnd },
 				});
-
 				const data = await queue.dequeueSkip<{ result: { items: [] } }>();
-				console.log(JSON.stringify(data["result"]["items"].slice(0, 10)));
+				// TODO: Time this
+				completionItems.items = data["result"]["items"];
 
-				// provideSuggestionItems();
+				for (let x = 0; x <= lemma.length; x++) {
+					// Promise which can be resolved when suggestions are done
+					const suggest = new Promise((res) => (suggestResolver.res = res));
 
-				console.log(
-					search,
-					tacticStart,
-					tacticEnd,
-					line[tacticEnd],
-					JSON.stringify([sentence, tactic, lemma])
-				);
+					const OFFSET = 2; // position in editor is 1-indexed, and include space
+					editor.setPosition(new Position(i + 1, tacticEnd + x + OFFSET));
+
+					controller.triggerSuggest();
+					await suggest;
+					type Cast = {
+						_completionModel: typeof controller.model["_completionModel"];
+					};
+					const topTen = (
+						controller.model as unknown as Cast
+					)._completionModel?.items
+						.slice(0, 10)
+						.map(({ textLabel }) => textLabel);
+					console.log("result: ", topTen);
+				}
+
+				// console.log(
+				// 	search,
+				// 	tacticStart,
+				// 	tacticEnd,
+				// 	JSON.stringify([_sentence, tactic, lemma]),
+				// 	line.slice(tacticEnd)
+				// );
 
 				search = line.slice(++tacticStart).search(regex);
 			}
-			// const m = line.match(regex) || [];
-			// m.forEach((x) => console.log(x));
 		}
 
 		await new Promise((res, rej) =>
@@ -270,19 +335,5 @@ suite("Test algorithms", function () {
 				res(null);
 			}, 1000)
 		);
-
-		const itemsNow = model.items;
-		let itemsThen = model.items;
-		assert.ok(itemsNow === itemsThen);
-
-		// still the same context
-		model.lineContext = { leadingLineContent: "foo", characterCountDelta: 0 };
-		itemsThen = model.items;
-		assert.ok(itemsNow === itemsThen);
-
-		// different context, refilter
-		model.lineContext = { leadingLineContent: "foo1", characterCountDelta: 1 };
-		itemsThen = model.items;
-		assert.ok(itemsNow !== itemsThen);
 	});
 });
