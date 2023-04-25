8fbc3a79ebb89f297ffb4da8dda3863f4bb806f2
Hjalte Sorgenfrei Mac Dalland <hjaltesorgenfrei@gmail.com>
2023-04-25 13:41:02 +0200

    Make 100 theorems

---
+++ makeDependecies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Make 100 Thereoms
+rm -f coq-100-theorems/cardan_ferrari.v
+
+# delete line containing "cardan_ferrari.v"
+(cd coq-100-theorems && sed -i '/cardan_ferrari.v/d' _CoqProject)
+
+(cd coq-100-theorems && coq_makefile -f _CoqProject -o Makefile && make)
