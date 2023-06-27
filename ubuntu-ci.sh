e7d6ced8c7985d5a0d5619fa675a14fd466cd1dd
Hjalte Sorgenfrei Mac Dalland <hjaltesorgenfrei@gmail.com>
2023-06-27 15:42:41 +0200

    Creating ci

--- ubuntu-ci.sh
+++ ubuntu-ci.sh
@@ -1,14 +1,18 @@
 export DEBIAN_FRONTEND=noninteractive
+# add node source
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 # Install dependencies
-
+sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
 apt update
-apt install -yq nodejs pkg-config make g++ libsecret-1-dev libx11-dev libxkbfile-dev gcc
-npm install -g yarn
+apt install -yq nodejs pkg-config make g++ libsecret-1-dev libx11-dev libxkbfile-dev gcc npm
+apt install -yq libatk1.0-0 libatk-bridge2.0-0 libcups2 libgtk-3-0 libgbm1 libasound2
+apt install -yq opam build-essential libgmp-dev
+
+# Switch user here. todo
 /bin/bash ./setup.sh
 /bin/bash ./build.sh
 
 # install vscoqtop
-apt install -yq opam build-essential
 opam init -a
 opam update
 opam switch create 4.13.1
@@ -18,7 +22,8 @@ opam pin add coq-stdlib.dev "https://github.com/coq/coq.git#51814505fdeb5bc9f11f
 opam pin add vscoq-language-server ./vscoq/language-server/ -y
 
 rm -rf ./out
-apt install -yq libatk1.0-0 libatk-bridge2.0-0 libcups2 libgtk-3-0 libgbm1 libasound2
 
 # Run run.sh as new user
+
+export DISPLAY=192.168.0.5:0.0
 
